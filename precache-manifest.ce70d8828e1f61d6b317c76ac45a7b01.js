91551f2f4cab0565d21d291f8b2e53afeadee5bd
AnkitCode99 <9999385728ankit@gmail.com>
2020-07-15 01:58:10 +0530

    Updates

---
+++ precache-manifest.ce70d8828e1f61d6b317c76ac45a7b01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8c6ccc09becce3bfc4d67a182e0353",
+    "url": "/React_image_gallery/index.html"
+  },
+  {
+    "revision": "ec5070e7dee76ef7969e",
+    "url": "/React_image_gallery/static/css/main.cda36a3e.chunk.css"
+  },
+  {
+    "revision": "e89ba99b980e223b965f",
+    "url": "/React_image_gallery/static/js/2.bdd47067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_image_gallery/static/js/2.bdd47067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5070e7dee76ef7969e",
+    "url": "/React_image_gallery/static/js/main.f76efe3e.chunk.js"
+  },
+  {
+    "revision": "20d526c1699639988427",
+    "url": "/React_image_gallery/static/js/runtime-main.216db88d.js"
+  }
+]);
